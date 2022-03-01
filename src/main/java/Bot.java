ed74f2278beeb63bf755a46a7f01aff0fcc657a9
Vladyslav <vladyslav.pjatkyvskyj@gmail.com>
2022-03-01 18:30:43 +0200

    initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,36 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.logging.Level;
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "VPWeatherBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5101676375:AAH59cn1NcMUqAaoahM4CIYWfPB_xgGFLWM";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/WeatherInfo.java
@@ -0,0 +1,12 @@
+public class WeatherInfo {
+    private final String API_CALL_TEMPLATE = "http://pro.openweathermap.org/data/2.5/forecast/hourly?";
+    private final String API_KEY_TEMPLATE = "bad4092dcb7625876d4f8721366d5e9c";
+    private String city;
+
+    public WeatherInfo(String city) {
+        this.city = city;
+    }
+
+    public  getWeatherForecast(){}
+
+}
